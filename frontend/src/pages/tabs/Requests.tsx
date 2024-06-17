1aabaf0bfa2f1f0031f0f0e636eb141c23fd4fb3
Bogus <boguskladik@gmail.com>
2024-06-17 12:08:17 +0300

    added requests

--- frontend/src/pages/tabs/Requests.tsx
+++ frontend/src/pages/tabs/Requests.tsx
@@ -1,7 +1,133 @@
-import React from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
+import {
+  Typography,
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
+import axios from 'axios';
+import CurrentUserContext from '../../context';
+import { useNavigate } from 'react-router-dom';
 
-const Requests = () => {
-  return <div>Requests</div>;
+// interface IRequest {
+//   id: number;
+//   studentName: string;
+//   courseName: string;
+// }
+
+interface IRequest {
+  mail: string;
+  surname: string;
+  firstname: string;
+  lastname: string;
+  course_name: string;
+}
+
+const Requests: FC = () => {
+  const [requests, setRequests] = useState<IRequest[]>([]);
+  const context = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!context || !context.isAuthAndToken) {
+      navigate('/signin');
+    } else {
+      axios
+        .post('//localhost:8080/unenrolled_courses', {
+          token: context.isAuthAndToken,
+        })
+        .then((response) => {
+          setRequests(response.data);
+        })
+        .catch((error) => {
+          console.error('Error: ', error);
+        });
+    }
+  });
+  // const fetchRequests = () => {
+  //   axios
+  //     .get('//localhost:8080/view_requests', {
+  //       headers: { Authorization: `Bearer ${context?.isAuthAndToken}` },
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //       setRequests(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching requests: ', error);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   fetchRequests();
+  // }, []);
+
+  const handleConfirm = (request: IRequest) => {
+    // axios
+    //   .post('//localhost:8080/confirm_request', {
+    //     requestId,
+    //     token: context?.isAuthAndToken,
+    //   })
+    //   .then(() => {
+    //     console.log('Request confirmed:', requestId);
+    //     fetchRequests();
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error confirming request: ', error);
+    //   });
+    if (context && context.isAuthAndToken) {
+      axios
+        .put('//localhost:8080/change_user_enrollment', {
+          mail: request.mail,
+          course_name: request.course_name,
+          enrollment: true,
+          token: context.isAuthAndToken,
+        })
+        .then((response) => {
+          console.log(response);
+          // fetchRequests();
+        })
+        .catch((error) => {
+          console.error('Error: ', error);
+        });
+    }
+  };
+
+  return (
+    <Box>
+      <Typography variant="h6" component="h2">
+        Requests
+      </Typography>
+      <Typography variant="body1">
+        Here is the list of all pending course requests.
+      </Typography>
+      <List>
+        {requests.map((request, index) => (
+          <ListItem key={index} divider>
+            <ListItemText
+              primary={
+                request.lastname +
+                ' ' +
+                request.firstname +
+                ' ' +
+                request.surname
+              }
+              secondary={request.course_name}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleConfirm(request)}
+            >
+              Confirm
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 };
 
 export default Requests;
